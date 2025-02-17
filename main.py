71c9f1fa615d102bcc2af30dabc187dea26ebb77
Osvaldx <romanonahuel24@gmail.com>
2025-02-17 17:07:00 -0300

    funcion $avatar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
+import discord.ext
 from discord.ext import commands
+import discord.ext.commands
+from packages.bot_token import TOKEN_BOT
 
 intents = discord.Intents.default()
 
@@ -7,3 +10,24 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="$",intents=intents)
+
+@bot.event
+async def on_ready():
+    print(" "*5 + f"[+] BOT ENCENDIDO: {bot.user}")
+
+@bot.command("avatar")
+async def que(ctx,usuario: str = None):
+    canal = ctx.channel # establecemos el canal donde estamos
+
+    if(usuario is None): # validamos que si no se envio nada automaticamente sea None y asi remplazarlo por el autor
+        usuario = ctx.author
+    else:
+        try: # intentamos convertir el mensaje a un Miembro
+            usuario = await commands.MemberConverter().convert(ctx=ctx,argument=usuario)
+        except: # en caso de q no sea un miembro se tira un mensaje de alerta
+            await canal.send("**[!] No se encontro al usuario**")
+            return
+
+    await canal.send(usuario.avatar.url) # se envia la foto del usuario
+
+bot.run(TOKEN_BOT)
