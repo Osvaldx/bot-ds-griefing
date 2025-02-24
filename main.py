0903b86a8822f789335776f6e7e0b41092f91b58
Osvaldx <romanonahuel24@gmail.com>
2025-02-24 02:50:00 -0300

    [+] Se agrego la informacion ASN de la IP

--- main.py
+++ main.py
@@ -37,20 +37,24 @@ async def info_serverMC(ctx,direccion: str = None):
         await ctx.send("**[ ! ]** *Ingrese una IP de MC*")
         return
     
-    datos = consulta_api_server(direccion) #Consultamos a la API para obtener toda la informacion de la IP
+    datos_sv_mc = consulta_api_server(direccion) #Consultamos a la API para obtener toda la informacion de la IP
+    datos_ip_sv = consultar_info_ip(datos_sv_mc['ip_address'])
     icono = f"https://api.mcstatus.io/v2/icon/{direccion}" #Obtenemos el ICONO del servidor
 
-    mensaje_uno = f"**IP:** {datos['ip_address']} \n**Puerto:** {datos['port']}"
-    mensaje_dos = f"**Players:** {datos['players']['online']}**/**{datos['players']['max']} \n**Version:** {datos['version']['name_clean']}"
-    mensaje_motd = f"**MOTD**:\n```{datos['motd']['clean']}```"
+    #Mensajes que van en el embed
+    mensaje_uno = f"**IP:** {datos_sv_mc['ip_address']} \n**Puerto:** {datos_sv_mc['port']}"
+    mensaje_dos = f"**Players:** {datos_sv_mc['players']['online']}**/**{datos_sv_mc['players']['max']} \n**Version:** {datos_sv_mc['version']['name_clean']}"
+    mensaje_tres = f"**ASN:** AS{datos_ip_sv['network']['autonomous_system']['asn']} \n**Organización:** {datos_ip_sv['network']['autonomous_system']['organization']}"
+    mensaje_motd = f"**MOTD**:\n```{datos_sv_mc['motd']['clean']}```"
 
     #Creamos el EMBED para enviar por discord con el formato
-    embed=discord.Embed(title="⌈ ɪɴꜰᴏʀᴍᴀᴄɪᴏ́ɴ ᴅᴇʟ ꜱᴇʀᴠɪᴅᴏʀ ⌋", description=f"~𝗰𝗼𝗻𝘀𝘂𝗹𝘁𝗮@: {datos['host']}")
+    embed=discord.Embed(title="⌈ ɪɴꜰᴏʀᴍᴀᴄɪᴏ́ɴ ᴅᴇʟ ꜱᴇʀᴠɪᴅᴏʀ ⌋", description=f"~𝗰𝗼𝗻𝘀𝘂𝗹𝘁𝗮@: {datos_sv_mc['host']}")
     embed.set_author(name="« SkullBOT | MC »", icon_url="https://media.discordapp.net/attachments/1213856557666795561/1342329848827482193/skullbot.jpg?ex=67b93d97&is=67b7ec17&hm=600b410ee73e715bc3bd8c85f27e0039427465b8edc82544d5f6754dd3d24a1c&=&format=webp&width=347&height=347")
     embed.set_thumbnail(url=icono)
     embed.add_field(name="▬▬▬▬▬▬▬▬▬▬▬▬▬▬", value="", inline=False)
     embed.add_field(name="",value=mensaje_uno, inline=True)
     embed.add_field(name="", value=mensaje_dos, inline=True)
+    embed.add_field(name="", value=mensaje_tres, inline=False)
     embed.add_field(name="", value=mensaje_motd, inline=False)
     embed.add_field(name="▬▬▬▬▬▬▬▬▬▬▬▬▬▬", value="", inline=False)
     embed.set_footer(text="github.com/Osvaldx")
--- packages/funciones.py
+++ packages/funciones.py
@@ -1,9 +1,16 @@
 import requests
 
 api_server_java = "https://api.mcstatus.io/v2/status/java/"
+api_informacion_ips = "https://ip.guide/"
 
 def consulta_api_server(direccion: str)-> dict | list:
     response = requests.get(api_server_java + direccion)
+    data = response.json()
+
+    return data
+
+def consultar_info_ip(direccion: str)->dict | list:
+    response = requests.get(api_informacion_ips + direccion)
     data = response.json()
 
     return data
