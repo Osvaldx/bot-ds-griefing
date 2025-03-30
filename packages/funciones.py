28f819da166b91b6db9b049fd8afe965d55bca94
Osvaldx <romanonahuel24@gmail.com>
2025-03-30 03:23:01 -0300

    [+] Se mejoro la validacion de nicknames

--- packages/funciones.py
+++ packages/funciones.py
@@ -45,7 +45,7 @@ def generar_uuids_jugador(nickname: str)->dict:
             nueva_uuid += char
         
         uuids_jugador['PremiumUUID'] = nueva_uuid
-    
+     
     return uuids_jugador
 
 def consultar_api_friends(uuid: str)->list:
@@ -142,6 +142,6 @@ def validar_consulta_ips(ip_ingresada: str)->bool:
 def validar_nicks(nickname:str)->bool:
     validado = False
     if(len(nickname) <= 16):
-        validado = True if(nickname.isalnum()) else False
+        validado = True if(char.isalnum() or char in "_" for char in nickname) else False
     
     return validado
